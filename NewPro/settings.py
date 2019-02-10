bf46ab8f84928c804773cc0d567081e7dcacff9c
Nikita <buschkoff@mail.ru>
2019-02-10 22:25:53 +0300

    Merge branch 'master' of https://github.com/amorid1234/my-first-blog

--- NewPro/settings.py
+++ NewPro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(*=4yf=qi8bns#^n(-b_8-c7vjp#)9n6)wl4s=pa%gm*^7t+jo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['amorid1234.pythonanywhere.com']
 
 
 # Application definition
