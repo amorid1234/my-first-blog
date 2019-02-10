8badc1b09533ef867b0dbfa9b4974fc2bdec6f5b
Nikita <buschkoff@mail.ru>
2019-02-10 14:50:20 +0300

    Внесли HTML Изменения

--- NewPro/settings.py
+++ NewPro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(*=4yf=qi8bns#^n(-b_8-c7vjp#)9n6)wl4s=pa%gm*^7t+jo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
