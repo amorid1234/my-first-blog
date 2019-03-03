996850d96977a85b7f3bbaf4bfff6f50ab71eb95
Nikita <buschkoff@mail.ru>
2019-03-03 18:00:53 +0300

    Added edit/new files

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+     url(r'^$', views.post_list, name='post_list'),
      url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+     url('post/new/', views.post_new, name='post_new'),
+     url('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
